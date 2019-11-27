ae67a1ccf79f85fbd6f4dce3199e778a87c5cc9a
Marcus <marcusfunctional01@gmailcom>
2019-11-27 15:37:42 +0000

    Added password

--- Automation.py
+++ Automation.py
@@ -7,10 +7,14 @@ def test_setup():
     global browser
     browser = webdriver.Firefox()  #
     browser.implicitly_wait(10)
-    browser.maximize_window()
+    #browser.maximize_window()
 
 
 def test_login():
     browser.get("https://opensource-demo.orangehrmlive.com/")
     browser.find_element_by_id("txtUsername").send_keys("Admin")
+
+    # Password
+    browser.find_element_by_id("txtPassword").send_keys("admin123")
+
 
