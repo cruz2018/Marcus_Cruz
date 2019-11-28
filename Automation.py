58edea7ef24cede343073fd1a89758ef595c7fc2
Marcus <marcusfunctional01@gmailcom>
2019-11-28 23:07:51 +0000

    I design my Unittest

--- Automation.py
+++ Automation.py
@@ -1,40 +1,29 @@
 import time
 
 from selenium import webdriver  #
-import unitest
+import unittest
 
-class LogingTest(unitest.TestCase):
+#class LogingTest(unitest.TestCase):
 
    
-
-    def test_setup(cls):
-        global browser
-        cls.browser = webdriver.Firefox()  #
-        cls.browser.implicitly_wait(10)
+def test_setup(cls):
+     global browser
+     cls.browser = webdriver.Firefox()  #
+     cls.browser.implicitly_wait(10)
         #browser.maximize_window()
         # Say something
         # Semething else
 
 
-def test_login():
-    browser.get("https://opensource-demo.orangehrmlive.com/")
-    browser.find_element_by_id("txtUsername").send_keys("Admin")
-
-    # Password
-    browser.find_element_by_id("txtPassword").send_keys("admin123")
-
-    # Click button
-    browser.find_element_by_id("btnLogin").click()
-
-    # The page will load and the user will land into the Welcome page
-
-    # Once the user are in the Welcome page than the user will logou from the system
-
-    browser.find_element_by_id("welcome").click()
+def test_login_valid(self):
+    self.browser.get("https://opensource-demo.orangehrmlive.com/")
+    self.browser.find_element_by_id("txtUsername").send_keys("Admin")
+    self.browser.find_element_by_id("txtPassword").send_keys("admin123")
+    self.browser.find_element_by_id("btnLogin").click()
+    self.browser.find_element_by_id("welcome").click()
     time.sleep(2)
 
-    # Close the browser
-    browser.close()
-    # quite 
-    browser.quit()
+def tearDownClass(cls):
+    cls.browser.close()
+    cls.browser.quit()
     print("Test Complete")
